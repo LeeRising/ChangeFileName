7919501d34b5e25ff66cb21e41985f0ee8c18c8c
Віталій Оверчук <LeeRising>
2017-09-12 22:54:49 +0300

    UpD

--- ChangeFileName/Program.cs
+++ ChangeFileName/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace ChangeFileName
 {
@@ -10,6 +8,36 @@ namespace ChangeFileName
     {
         static void Main(string[] args)
         {
+            const string renameFolder = "rename";//folder
+            try
+            {
+                if (!Directory.Exists(renameFolder))
+                    Directory.CreateDirectory(renameFolder);
+                else
+                {
+                    var renameFiles = Directory.GetFiles(renameFolder);
+                    foreach (var renameFile in renameFiles)
+                        File.Delete(renameFile);
+                }
+                var filesInFolder = Directory.GetFiles(Directory.GetCurrentDirectory());
+                foreach (var file in filesInFolder)
+                {
+                    var regex = new Regex(".png|.jpg", RegexOptions.IgnoreCase);
+                    using (var fileStream = new StreamReader(file))
+                    {
+                        if (!regex.IsMatch(Path.GetExtension(file))) continue;
+                        using (var writer = new StreamWriter("rename/"+Path.GetFileName(file).Replace('–', '_').Replace('-', '_')))
+                            writer.Write(fileStream);
+                    }
+                    Console.WriteLine($"{Path.GetFileName(file)} successfully renamed");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.ReadKey();
         }
     }
 }
